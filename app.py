694941af3ab8d26ed959e8489ac02a75cd620dc9
vandana s <vandanasavantanavar2001@gmail.com>
2022-12-13 13:51:23 +0530

    logging and exception done

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
+import sys
 app=Flask(__name__)
 
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    try:
+        raise Exception('We are testing custom exception')
+    except Exception as e:
+        housing=HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("we are testing")
     return "vandanaa"
   
   
